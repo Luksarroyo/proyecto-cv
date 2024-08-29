0c625fca62b89ff9317e702e4e167d0387c7eb27
Luksarroyo <120147360+Luksarroyo@users.noreply.github.com>
2024-08-29 15:01:16 +0100

    arreglo contador detalle procuto

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import style from "./Counter.module.css";
-const Counter = ({ counter, sumar, restar, onAdd }) => {
+const Counter = ({ counter, sumar, restar, onAdd, errorMessage }) => {
   return (
     <div className={style.counterContainer}>
       <div className={style.counter}>
         <button onClick={restar}> - </button>
         <h3>{counter}</h3>
         <button onClick={sumar}>+</button>
+        {errorMessage ? <p style={{ color: "white" }}>{errorMessage}</p> : ""}
       </div>
-      <button onClick={() => onAdd(counter)}>Agragar al carrito</button>
+
+      <button onClick={() => onAdd(counter)}>Agregar al carrito</button>
     </div>
   );
 };
--- src/components/Counter/CounterContainer.jsx
+++ src/components/Counter/CounterContainer.jsx
@@ -3,25 +3,32 @@ import Counter from "./Counter";
 // 5to recibo la funcion on add de detalleProducto
 const CounterContainer = ({ stock, onAdd }) => {
   const [counter, setCounter] = useState(1);
-
+  const [errorMessage, setErrorMessage] = useState();
   const sumar = () => {
     if (counter < stock) {
       setCounter(counter + 1);
+      setErrorMessage("");
     } else {
-      alert("stock insuficiente");
+      setErrorMessage("Stock insuficiente");
     }
   };
-
   const restar = () => {
     if (counter > 1) {
       setCounter(counter - 1);
+      setErrorMessage("");
     } else {
-      alert("Error");
+      setErrorMessage("Stock insuficiente");
     }
   };
   return (
     <div>
-      <Counter counter={counter} sumar={sumar} restar={restar} onAdd={onAdd} />
+      <Counter
+        counter={counter}
+        sumar={sumar}
+        restar={restar}
+        onAdd={onAdd}
+        errorMessage={errorMessage}
+      />
     </div>
   );
 };
